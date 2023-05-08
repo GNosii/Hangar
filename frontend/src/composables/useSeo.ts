2dadb4a80d0360464d99a233a1a6dc09845219a9
Gabriel Arriagada <65878291+GNosii@users.noreply.github.com>
2023-05-07 19:01:58 -0600

    change manifest reference in meta tags

--- frontend/src/composables/useSeo.ts
+++ frontend/src/composables/useSeo.ts
@@ -42,7 +42,7 @@ export function useSeo(
       { rel: "apple-touch-icon", sizes: "180x180", href: "/favicon/apple-touch-icon.png" },
       { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon/favicon-32x32.png" },
       { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon/favicon-16x16.png" },
-      { rel: "manifest", href: "/favicon/site.webmanifest" },
+      { rel: "manifest", href: "/manifest.json" },
       { rel: "mask-icon", href: "/favicon/safari-pinned-tab.svg", color: "#686868" },
       { rel: "shortcut icon", href: "/favicon/favicon.ico" },
     ],
